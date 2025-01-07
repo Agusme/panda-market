46ac958a281d25b86fef8c1cf3f5a712ace5ce5c
agusmena <agusmena60@mail.com>
2025-01-07 15:40:09 -0300

    changes into home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ import Nav from "react-bootstrap/Nav";
 import logoPanda from "../assets/panda.png";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
-import productoEstrella from "../assets/producto-estrella.jpg"
 const Header = () => {
   return (
     <>
@@ -39,31 +38,7 @@ const Header = () => {
       </Navbar>
       <div className="hr-pink p-2"></div>
 
-      <Container className="bg-header mb-4" style={{ minHeight: "300px" }}>
-        <Row className="h-100">
-          <Col sm={12} md={6} lg={6}>
-            <div className="recuadro-img p-2 margin-titulo d-flex justify-content-center w-75 mx-auto mb-5">
-<img src={productoEstrella} alt="producto estrella" className="img-fluid" />
-            </div>
-          </Col>
-          <Col sm={12} md={6} lg={6} className="font-family-header d-flex ">
-            <div className="w-75">
-              <h2 className="fw-bold margin-titulo">
-                {" "}
-                <span className="color-yellow"> Let  </span>
-                <span className="color-pink">p</span>
-                <span className="color-orange">a</span>
-                <span className="color-yellow">n</span>
-                <span className="color-aqua">d</span>
-                <span className="color-lightPink">a</span>
-                <span className="fontfamily-market"> market </span>  
-                <span className="color-aqua">add joy to your daily routine! </span>
-              </h2>
-              <button className="btn-pink lead fs-6 px-4 py-2 my-5" >Learn More</button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+   
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,44 @@ import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import productoEstrella from "../assets/producto-estrella.jpg";
 const Home = () => {
   return (
     <>
       <Header></Header>
+      <Container className="bg-header mb-4" style={{ minHeight: "300px" }}>
+        <Row className="h-100">
+          <Col sm={12} md={6} lg={6}>
+            <div className="recuadro-img p-2 margin-titulo d-flex justify-content-center w-75 mx-auto mb-5">
+              <img
+                src={productoEstrella}
+                alt="producto estrella"
+                className="img-fluid"
+              />
+            </div>
+          </Col>
+          <Col sm={12} md={6} lg={6} className="font-family-header d-flex ">
+            <div className="w-75">
+              <h2 className="fw-bold margin-titulo">
+                {" "}
+                <span className="color-yellow"> Let </span>
+                <span className="color-pink">p</span>
+                <span className="color-orange">a</span>
+                <span className="color-yellow">n</span>
+                <span className="color-aqua">d</span>
+                <span className="color-lightPink">a</span>
+                <span className="fontfamily-market"> market </span>
+                <span className="color-aqua">
+                  add joy to your daily routine!{" "}
+                </span>
+              </h2>
+              <button className="btn-pink lead fs-6 px-4 py-2 my-5">
+                Learn More
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
       <Container className="hr-pink" fluid>
         <Container className="text-center mt-2 my-4">
           <Row>
